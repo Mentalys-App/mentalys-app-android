29639a6f824fda7c5db66ab0e7f95a114422919c
IMadeAgus <agsmade@gmail.com>
2024-11-12 20:32:36 +0800

    feat(homepage) set up RecyclerView for clinic and psikiater
    - load dummy data for initial testing

--- app/src/main/java/com/mentalys/app/ui/fragments/HomeFragment.kt
+++ app/src/main/java/com/mentalys/app/ui/fragments/HomeFragment.kt
@@ -5,10 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CircleCrop
 import com.mentalys.app.R
 import com.mentalys.app.databinding.FragmentHomeBinding
+import com.mentalys.app.ui.adapters.ClinicAdapter
+import com.mentalys.app.ui.adapters.ClinicItem
+import com.mentalys.app.ui.adapters.PsychiatristsAdapter
+import com.mentalys.app.ui.adapters.PsychiatristsItem
 
 class HomeFragment : Fragment() {
 
@@ -41,6 +46,56 @@ class HomeFragment : Fragment() {
             .load(R.drawable.ic_launcher_background)
             .transform(CircleCrop())
             .into(binding.iconMenuBlm)
+
+        val clinicItems = listOf(
+            ClinicItem(
+                R.drawable.img_clinic,
+                "Psikiater DR. Dr. Anak Ayu Sri Wahyuni, SpKJ",
+                "5.00–8.00 pm",
+                "Jl. Belimbing No.74, Dangin Puri Kaja, Kec. Denpasar Utara, Kota Denpasar, Bali 80234"
+            ),
+            ClinicItem(
+                R.drawable.img_clinic,
+                "Psikiater DR. Dr. Anak Ayu Sri Wahyuni, SpKJ",
+                "5.00–8.00 pm",
+                "Jl. Belimbing No.74, Dangin Puri Kaja, Kec. Denpasar Utara, Kota Denpasar, Bali 80234"
+            ),
+            ClinicItem(
+                R.drawable.img_clinic,
+                "Psikiater DR. Dr. Anak Ayu Sri Wahyuni, SpKJ",
+                "5.00–8.00 pm",
+                "Jl. Belimbing No.74, Dangin Puri Kaja, Kec. Denpasar Utara, Kota Denpasar, Bali 80234"
+            )
+        )
+        val clinicAdapter = ClinicAdapter(clinicItems)
+        binding.rvNearbyClinics.layoutManager =
+            LinearLayoutManager(activity, LinearLayoutManager.HORIZONTAL, false)
+        binding.rvNearbyClinics.adapter = clinicAdapter
+
+        val psychiatristsItem = listOf(
+            PsychiatristsItem(
+                R.drawable.img_clinic,
+                "Dr Made Agus Buydiartaaaa",
+                "08.00 - 18.00",
+                "Rp 20.000,00"
+            ),
+            PsychiatristsItem(
+                R.drawable.img_clinic,
+                "Dr Made Agus",
+                "08.00 - 18.00",
+                "Rp 20.000,00"
+            ),
+            PsychiatristsItem(
+                R.drawable.img_clinic,
+                "Dr Made Agus",
+                "08.00 - 18.00",
+                "Rp 20.000,00"
+            ),
+        )
+        val psychiatristsAdapter = PsychiatristsAdapter(psychiatristsItem)
+        binding.rvPsychiatrists.layoutManager =
+            LinearLayoutManager(activity, LinearLayoutManager.HORIZONTAL, false)
+        binding.rvPsychiatrists.adapter = psychiatristsAdapter
     }
 
     override fun onDestroyView() {
