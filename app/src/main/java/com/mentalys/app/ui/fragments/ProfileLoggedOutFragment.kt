cbef4d5b4d056be683a2d32f00afa256cfaa6a59
zyrridian <rezkyaf246@gmail.com>
2024-11-23 22:02:46 +0700

    feat(ui): add profile logged out screen layout

---
+++ app/src/main/java/com/mentalys/app/ui/fragments/ProfileLoggedOutFragment.kt
@@ -0,0 +1,40 @@
+package com.mentalys.app.ui.fragments
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.mentalys.app.databinding.FragmentProfileLoggedOutBinding
+import com.mentalys.app.ui.activities.SettingsActivity
+
+class ProfileLoggedOutFragment : Fragment() {
+
+    private var _binding: FragmentProfileLoggedOutBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentProfileLoggedOutBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.settingsImageView.setOnClickListener {
+            val intent = Intent(activity, SettingsActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
